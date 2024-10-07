fb0eb07e813d19eb94ef9723d1a9cff026169358
Juli Kumari <117572954+julikray@users.noreply.github.com>
2024-10-07 12:15:37 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
 
- const url = "http://localhost:4000"
+ const url = "https://food-delivery-backend-2nfa.onrender.com"
 
   return (
     <div>
